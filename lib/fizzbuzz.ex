f83f8c03fdf48b37ea5fcfb7f7e24025470d1c3e
Akil Krishnan <akilram@gmail.com>
2016-10-15 00:25:58 -0500

    document functions
    Add spec and doc for functions, some minor spacing cleanup

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,4 +1,8 @@
 defmodule Fizzbuzz do
+
+  @doc """
+  Runs the fizzbuzz program from 1 to 100, then prints the result
+  """
   def start do
     for {arg, result} <- FizzbuzzLib.fizzbuzz(1, 100),
       do: IO.puts Integer.to_string(arg) <> ": " <> result
--- lib/fizzbuzz_lib.ex
+++ lib/fizzbuzz_lib.ex
@@ -1,10 +1,20 @@
 defmodule FizzbuzzLib do
 
+  @doc """
+  Process numbers from a to b with fizzbuzz logic. Returns a proplist of the
+  number as the key, and the fizzbuzz string result
+  """
+  @spec fizzbuzz(number, number) :: {number, String.t}
   def fizzbuzz(a, b) when is_integer(a) and is_integer(b) do
     for n <- a..b, do: {n, to_fizzbuzz(n)}
   end
 
-
+  @doc """
+  Takes a number and converts it to "fizz" if it is a multiple of 3, "buzz" if
+  it is a multiple of 5, "fizzbuzz" if it is a multiple of 3 and also 5, or the
+  number as a string
+  """
+  @spec to_fizzbuzz(number) :: String.t
   def to_fizzbuzz(n) when rem(n, 3) == 0 do
     if rem(n, 5) == 0, do: "fizzbuzz", else: "fizz"
   end
